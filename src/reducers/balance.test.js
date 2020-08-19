54cb29e6d426dcc2ad19505a917142d8d8b2bf04
Barri <info@bdesignstudio.co.uk>
2020-08-19 17:05:24 +0100

    Add test for reducer for deposit

--- src/reducers/balance.test.js
+++ src/reducers/balance.test.js
@@ -9,4 +9,10 @@ describe('balanceReducer', () =>{
     expect(balanceReducer(undefined, { type: constants.SET_BALANCE, balance}))
     .toEqual(balance)
   }); 
+
+  it('accept a deposits into balance ', ()=>{
+    const deposit = 10;
+    const initialState = 5;
+    expect(balanceReducer(  initialState , { type: constants.DEPOSIT, deposit })).toEqual(initialState + deposit);
+  })
 });
