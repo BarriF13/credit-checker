e67864d3adcc3cd8f78784f2b097530e0c05c0c3
Barri <info@bdesignstudio.co.uk>
2020-08-20 17:57:18 +0100

    Pass the test

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -9,12 +9,14 @@ export class Wallet extends Component {
     this.state = { balance: undefined }
   }
   updateBalance = event => this.setState({ balance: parseInt(event.target.value, 10) })
+  deposit =()=> this.props.deposit(this.state.balance)
   render() {
     return (
       <div>
         <h3 className='balance'>Wallet balance: { this.props.balance }</h3>
         <br/>
         <input className="input-wallet" onChange={this.updateBalance}/>
+        <button className="btn-deposit" onClick={this.deposit}></button>
       </div>
     )
   }
