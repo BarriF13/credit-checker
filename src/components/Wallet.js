ce8ed4b9fcb551e4eb91e8ab45fcdf4707748bdd
Barri <info@bdesignstudio.co.uk>
2020-08-20 18:08:13 +0100

    pass test for withdraw

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -9,17 +9,19 @@ export class Wallet extends Component {
     this.state = { balance: undefined }
   }
   updateBalance = event => this.setState({ balance: parseInt(event.target.value, 10) })
-  deposit = ()=> this.props.deposit(this.state.balance)
+  deposit = ()=> this.props.deposit(this.state.balance);
+  withdraw = ()=> this.props.withdraw(this.state.balance);
   render() {
     return (
       <div>
         <h3 className='balance'>Wallet balance: { this.props.balance }</h3>
         <br/>
         <input className="input-wallet" onChange={this.updateBalance}/>
-        <button className="btn-deposit" onClick={this.deposit}>add deposit</button>
+        <button className="btn-deposit" onClick={this.deposit}>Deposit</button>
+        <button className="btn-withdraw" onClick={this.withdraw}>Withdraw</button>
       </div>
     )
   }
 }
 
-export default connect( state => {return {balance : state} }, { deposit} )(Wallet);
+export default connect( state => {return {balance : state} }, { deposit, withdraw} )(Wallet);
