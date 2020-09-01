8b538591f5e43c0c5d6de3552a11f6dff858be81
Barri <info@bdesignstudio.co.uk>
2020-09-01 18:52:59 +0100

    Test credit component

---
+++ src/components/credit.test.js
@@ -0,0 +1,17 @@
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+import React, { Component } from 'react';
+
+import { shallow } from 'enzyme';
+import { Credit } from './Credit'
+
+describe('Credit', () => {
+  const credit = shallow(<Credit />);
+
+  it ('renders properly', ()=>{
+    expect(credit).toMatchSnapShot();
+  })
+})
